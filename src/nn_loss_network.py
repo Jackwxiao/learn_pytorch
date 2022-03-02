ae752dfcd7cb630def5174832eb4505e4914db0f
wuxiao <xiaopig0106@gmail.com>
2022-03-02 15:11:08 +0800

    模型调优 optim

--- src/nn_loss_network.py
+++ src/nn_loss_network.py
@@ -1,3 +1,4 @@
+import torch.optim
 import torchvision
 from torch import nn
 from torch.nn import Sequential, Conv2d, MaxPool2d, Flatten, Linear
@@ -32,7 +33,16 @@ class Wxiao(nn.Module):
 
 loss = nn.CrossEntropyLoss()# 交叉熵损失函数
 wxiao = Wxiao()
-for data in dataloader:
-    imgs, targets = data
-    outputs = wxiao(imgs)
-    result_loss = loss(outputs, targets)# 神经网络输出和真实值的误差
+# 定义优化器
+optim = torch.optim.SGD(wxiao.parameters(), lr=0.01)# lr 为学习速率
+for epoch in range(20): # 循环20次
+    running_loss = 0.0
+    for data in dataloader:
+        imgs, targets = data
+        outputs = wxiao(imgs)
+        result_loss = loss(outputs, targets)# 神经网络输出和真实值的误差
+        optim.zero_grad() # 梯度清零
+        result_loss.backward() # 反向传播计算梯度
+        optim.step()# 模型参数调优
+        running_loss = running_loss + result_loss
+    print(running_loss)
