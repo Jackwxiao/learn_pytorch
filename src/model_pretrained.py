12b909cf60709af89304fa271a3c3f1355338a79
wuxiao <xiaopig0106@gmail.com>
2022-03-02 16:06:21 +0800

    修改最后一层的输出

--- src/model_pretrained.py
+++ src/model_pretrained.py
@@ -8,6 +8,7 @@ vgg16_true = torchvision.models.vgg16(pretrained=True)
 
 train_data = torchvision.datasets.CIFAR10("../dataset", train=True, transform=torchvision.transforms.ToTensor(), download=True)
 
-vgg16_true.classifier.add_module('add_linear', nn.Linear(1000, 10))# 添加一个线性层
+vgg16_true.classifier.add_module('add_linear', nn.Linear(1000, 10))# 添加一个线性层   (6): Linear(in_features=4096, out_features=1000, bias=True)
 print(vgg16_true)
-# vgg16_false.add_module('add_linear', nn.Linear(1000, 10))
+vgg16_false.add_module('add_linear', nn.Linear(1000, 10))# 修改最后一层的输出为10，out_features=10
+print(vgg16_false)
