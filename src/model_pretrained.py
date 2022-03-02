dd61b7936e95ef2f3844651266cae8f3bc52a2f2
wuxiao <xiaopig0106@gmail.com>
2022-03-02 16:01:32 +0800

    在classfier中添加一个线性层

--- src/model_pretrained.py
+++ src/model_pretrained.py
@@ -8,6 +8,6 @@ vgg16_true = torchvision.models.vgg16(pretrained=True)
 
 train_data = torchvision.datasets.CIFAR10("../dataset", train=True, transform=torchvision.transforms.ToTensor(), download=True)
 
-vgg16_true.add_module('add_linear', nn.Linear(1000, 10))# 添加一个线性层
+vgg16_true.classifier.add_module('add_linear', nn.Linear(1000, 10))# 添加一个线性层
 print(vgg16_true)
 # vgg16_false.add_module('add_linear', nn.Linear(1000, 10))
