fc3291ef9eb70178b550d481f8f45f179c4b59df
吴潇 <xiaopig0106@gmail.com>
2022-03-03 19:06:24 +0800

    完整的模型训练套路

---
+++ src/model_wxiao.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+
+
+class Wxiao(nn.Module):
+    def __init__(self):
+        super(Wxiao, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+if __name__ == '__main__':
+    wxiao = Wxiao()
+    input = torch.ones(64, 3, 32, 32)
+    output = wxiao(input)
+    print(output.shape)
+
---
+++ src/train.py
@@ -0,0 +1,86 @@
+import torchvision.datasets
+from torch import nn
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model_wxiao import *
+
+
+# 准备数据集
+train_data = torchvision.datasets.CIFAR10("../dataset", train=True, transform=torchvision.transforms.ToTensor(), download=True)
+test_data = torchvision.datasets.CIFAR10("../dataset", train=False, transform=torchvision.transforms.ToTensor(), download=True)
+
+train_data_size = len(train_data)
+test_data_size = len(test_data)
+print("训练数据集的长度为： {}".format(train_data_size))
+print("测试数据集的长度为： {}".format(test_data_size))
+
+# 加载数据
+train_data_loader = DataLoader(train_data, batch_size=64)
+test_data_loader = DataLoader(test_data, batch_size=64)
+
+# 定义网络模型
+# 创建网络
+wxiao = Wxiao()
+
+# 损失函数
+loss_fun = nn.CrossEntropyLoss()
+
+# 优化器
+learning_rate = 1e-2
+optimzer = torch.optim.SGD(wxiao.parameters(), lr=learning_rate)
+
+# 设置训练网络的参数
+# 次数
+total_train_step = 0 # 训练次数
+total_test_step = 0 # 测试次数
+# 训练轮数
+epoch = 10
+
+#  添加tensorboard
+writer = SummaryWriter("../logs_train")
+
+for i in range(epoch):
+    print("第{}轮训练开始".format(i+1))
+    # 训练
+    wxiao.train()
+    for data in train_data_loader:
+        imgs, targets = data
+        outputs = wxiao(imgs)
+        loss = loss_fun(outputs, targets)
+
+        # 优化调优
+        optimzer.zero_grad()
+        loss.backward()
+        optimzer.step()
+
+        total_train_step = total_train_step + 1
+        if total_train_step % 100 == 0:
+            print("训练次数: {}, Loss: {}".format(total_train_step, loss.item()))
+            writer.add_scalar("train_loss", loss.item(), total_train_step)
+
+# 测试
+    wxiao.eval()
+    total_test_loss = 0
+    total_accuracy = 0
+    with torch.no_grad(): #测试不需要梯度
+        for data in test_data_loader:
+            imgs, targets = data
+            outputs = wxiao(imgs)
+            loss = loss_fun(outputs, targets)
+            total_test_loss = total_test_loss + loss.item()
+            accuracy = (outputs.argmax(1) == targets).sum() #求每个对应位置的最大值，再与真实输入相比较，得true or false 即0和1，再求和
+            total_accuracy = total_accuracy + accuracy # 整体测试集上的正确率
+    print("整体测试集上的Loss: {}".format(total_test_loss))
+    print("整体测试集上的正确率: {}".format(total_accuracy/test_data_size))
+    writer.add_scalar("test_loss", total_test_loss, total_test_step)
+    writer.add_scalar("test_accuracy", total_accuracy/test_data_size, total_test_step)
+    total_test_step = total_test_step + 1
+
+    torch.save(wxiao, "wxiao_{}.pth".format(i)) # 保存模型
+    # torch.save(wxiao.state_dict(), "wxiao_{}.pth.pth")
+
+writer.close()
+
+
+
